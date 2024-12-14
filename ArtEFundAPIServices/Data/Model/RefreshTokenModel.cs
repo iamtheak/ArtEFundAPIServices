21b3f3642740ec1bfedac0dcdbaad2cc8d761371
Adarsha Karki <np03cs4a220100@heraldcollege.edu.np>
2024-12-14 15:35:17 +0545

    Add Changes for refresh token and complete login and register feature

---
+++ ArtEFundAPIServices/Data/Model/RefreshTokenModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArtEFundAPIServices.Data.Model;
+
+public class RefreshTokenModel
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int RefreshTokenId { get; set; }
+    
+    public string Token { get; set; }
+    
+    public DateTime Expires { get; set; }
+    
+    public bool IsRevoked { get; set; }
+    public int UserId { get; set; }
+    
+    //Navigation Property
+    public UserModel User { get; set; }
+}
---
+++ ArtEFundAPIServices/Mapper/UserMapper.cs
@@ -0,0 +1,44 @@
+﻿using ArtEFundAPIServices.Data.Model;
+using ArtEFundAPIServices.DTO.Auth;
+
+namespace ArtEFundAPIServices.Mapper;
+
+public static class UserMapper
+{
+    public static UserModel ToUserModel(UserCreateDto userDto)
+    {
+        return new UserModel()
+        {
+            UserName = userDto.UserName,
+            Email = userDto.Email,
+            FirstName = userDto.FirstName,
+            LastName = userDto.LastName,
+            PasswordHash = userDto.Password
+        };
+    }
+
+    public static UserCreateDto ToUserCreateDto(UserModel userModel)
+    {
+        return new UserCreateDto()
+        {
+            UserName = userModel.UserName,
+            Email = userModel.Email,
+            FirstName = userModel.FirstName,
+            LastName = userModel.LastName,
+            Password = userModel.PasswordHash 
+        };
+    }
+
+    public static UserViewDto ToUserViewDto(UserModel userModel)
+    {
+        return new UserViewDto()
+        {
+            UserId = userModel.UserId,
+            UserName = userModel.UserName,
+            Email = userModel.Email,
+            FirstName = userModel.FirstName,
+            LastName = userModel.LastName,
+            Roles = userModel.UserRoles.Any() ? userModel.UserRoles.Select(x => x.RoleModel.RoleName).ToArray() : []
+        };
+    }
+}

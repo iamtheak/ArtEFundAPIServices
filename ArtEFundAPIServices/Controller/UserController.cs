69d9aeff07a8c48bdfb13bb5d79cec2bc6a5b571
Adarsha Karki <np03cs4a220100@heraldcollege.edu.np>
2025-01-04 17:49:32 +0545

    Add database migrations, attribute to check roles and user controller

---
+++ ArtEFundAPIServices/Controller/UserController.cs
@@ -0,0 +1,28 @@
+﻿using ArtEFundAPIServices.DataAccess.User;
+using ArtEFundAPIServices.DTO.Auth;
+using ArtEFundAPIServices.Mapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtEFundAPIServices.Controller;
+
+[Authorize]
+[ApiController]
+[Route("/api/[controller]")]
+[Consumes("application/json")]
+public class UserController(IUserInterface _userInterface) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var users = await _userInterface.GetAllUsers();
+        
+        List<UserViewDto> userViewDtos = new List<UserViewDto>();
+        
+        foreach (var user in users)
+        {
+           userViewDtos.Add(UserMapper.ToUserViewDto(user));
+        }
+        return Ok(userViewDtos);
+    }
+}
---
+++ ArtEFundAPIServices/Data/DbMigrations/20250103132601_RemoveUserRoles.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtEFundAPIServices.Data.DbMigrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUserRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_Users_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropTable(
+                name: "UserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoleId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserModelUserId",
+                table: "RefreshTokens",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_UserModelUserId",
+                table: "RefreshTokens",
+                column: "UserModelUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_Users_UserModelUserId",
+                table: "RefreshTokens",
+                column: "UserModelUserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "RoleId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_Users_UserModelUserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_UserModelUserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserModelUserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.CreateTable(
+                name: "UserRoles",
+                columns: table => new
+                {
+                    UserRoleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoles", x => x.UserRoleId);
+                    table.ForeignKey(
+                        name: "FK_UserRoles_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "RoleId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserRoles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshTokens",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_RoleId",
+                table: "UserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_UserId",
+                table: "UserRoles",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_Users_UserId",
+                table: "RefreshTokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
